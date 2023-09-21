7e77a65a06d12a23d3d4169440d220a2201552bb
angelAntezana <angel2estevan@gmail.com>
2023-09-21 18:57:12 +0200

    tiempo_app_demo completada

---
+++ src/components/CityList.vue
@@ -0,0 +1,60 @@
+<template>
+    <div v-for="city in savedCities"
+    :key="city.id"
+    >
+        <CityCard :city="city" @click="gotToCityView(city)">
+
+        </CityCard>
+    </div>
+
+    <p v-if="savedCities.length===0">
+        No location added. To start tracking a location, search in the field above.
+    </p>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import CityCard from './CityCard.vue';
+
+const LOCAL_SAVED_CITIES = 'savedCities';
+const BASE_URL_WEATHER = import.meta.env.VITE_baseurl_weather;
+const API_KEY = import.meta.env.VITE_apikey;
+
+const savedCities = ref([]);
+const getCities = async ()=>{
+    if(localStorage.getItem(LOCAL_SAVED_CITIES)){
+        savedCities.value = JSON.parse(
+            localStorage.getItem(LOCAL_SAVED_CITIES)
+            );
+    const requests = [];
+    savedCities.value.forEach((city)=>{
+        requests.push(
+            axios.get(`${BASE_URL_WEATHER}?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=${API_KEY}&units=metric`)
+        );
+    });
+
+    const weatherData = await Promise.all(requests);
+
+    // Flicker Delay
+    await new Promise((res)=> setTimeout(res,1000));
+    
+    weatherData.forEach((value,index)=>{
+        savedCities.value[index].weather = value.data;
+    })
+
+    }
+};
+await getCities();
+
+const router = useRouter();
+const gotToCityView = (city)=>{
+    router.push({
+        name:"cityView",
+        params:{state:city.state, city: city.city},
+        query: {id:city.id, lat: city.coords.lat, lng: city.coords.lng},
+    })
+}
+</script>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,96 @@
+<script setup>
+import axios from 'axios';
+import { ref } from "vue";
+import { useRouter } from 'vue-router';
+import CityCardSkeleton from '../components/CityCardSkeleton.vue';
+import CityList from '../components/CityList.vue';
+
+const router = useRouter();
+const previewCity = (searchResult) =>{
+  console.log("DATOS DEL MAPA API CIUDAD DESDE HOMEVIEW")
+  console.log(searchResult)
+  const [city,state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: {state: state.replaceAll(" ",""), city:city},
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true
+    }
+  })
+}
+
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null)
+
+
+const mapboxAPIKey = "pk.eyJ1IjoiYW5nZWwtYW50ZXphbmEiLCJhIjoiY2xtcGJvano4MDN3ZDJrcW1wY3RhcjRiMyJ9.qKRaMGbCBAOjXsA0U8ldXw"
+const getSearchResults = ()=>{
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async ()=>{
+    if(searchQuery.value !== ""){
+      try {
+        const result = await axios.get(`
+        https://api.mapbox.com/geocoding/v5/mapbox.places/
+        ${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`);
+        mapboxSearchResults.value= result.data.features;
+      } catch (error) {
+        searchError.value = true;
+      }
+   
+        return;
+    }
+    mapboxSearchResults.value = null;
+  },300);
+}
+</script>
+
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        name=""
+        id=""
+        placeholder="Search for a city"
+        class="py-2 px-1 w-full bg-transparent border-b
+        focus:border-weather-secondary
+        focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+      class="absolute bg-weather-secondary text-white
+      w-full shadow-md py-2 px-1 top-[66px]"
+      v-if="mapboxSearchResults"
+      >
+      <p v-if="searchError">Sorry, something went wrong,please try again.
+      </p>
+      <p v-if="!searchError && mapboxSearchResults.length ===0">
+        No results match your query, try a different term.
+      </p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+          @click="previewCity(searchResult)"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
+
+      </ul>
+    </div>
+    <div class="flex flex-col gap-4 ">
+      <Suspense>
+        <CityList/>
+          <template #fallback>
+            <CityCardSkeleton></CityCardSkeleton>
+          </template>
+      </Suspense>
+    </div>
+  </main>
+</template>
